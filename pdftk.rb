3544fbcd24d5902548328650d6b90f758bfab242
Claudio Beatrice <claudi0.beatric3@gmail.com>
2016-12-05 16:12:01 +0100

    changed sha1 to sha256

--- pdftk.rb
+++ pdftk.rb
@@ -13,7 +13,7 @@ class Pdftk < Formula
   homepage 'http://www.pdflabs.com/tools/pdftk-server'
   url 'http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.6-setup.pkg',
     :using => PkgExtract
-  sha1 '93226ecef996d7ad833a773ee15f9b62365fb567'
+  sha256 '93226ecef996d7ad833a773ee15f9b62365fb567'
 
   depends_on :macos => :lion
 
